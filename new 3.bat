685cac93da1a958e9940d267fabdf64078dd85a4
Jannik Davidsen <shyeinua@gmail.com>
2016-11-21 14:58:51 +0100

    script testing

--- new 3.bat
+++ new 3.bat
@@ -1,6 +1,5 @@
-cd "c:/Users/Jannik/projects/github-demo"
 git add -A --all
-echo "Indtast commit besked"
+echo Indtast commit besked
 set /p message=
 git commit -m "%message%"
 git push origin master
